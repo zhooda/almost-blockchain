d82b4966b62d32f1ec9c3f473983a8490f2688cf
Zeeshan Hooda <z.hooda@share.epsb.ca>
2019-06-04 11:50:37 -0600

    Added new "learning" projects for the new programatic UI features in SwiftUI and Xcode 11 beta.

---
+++ Sources/Learning/swift-programatic-ui/swift-programatic-ui/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  swift-programatic-ui
+//
+//  Created by Zeeshan Hooda on 6/4/19.
+//  Copyright © 2019 Deceptive Labs. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage : View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+#if DEBUG
+struct SwiftUIView_Previews : PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+#endif
---
+++ Sources/Learning/swift-programatic-ui/swift-programatic-ui/MapView.swift
@@ -0,0 +1,31 @@
+//
+//  MapView.swift
+//  swift-programatic-ui
+//
+//  Created by Zeeshan Hooda on 6/4/19.
+//  Copyright © 2019 Deceptive Labs. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView : UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+    }
+}
+
+#if DEBUG
+struct MapView_Previews : PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+#endif
