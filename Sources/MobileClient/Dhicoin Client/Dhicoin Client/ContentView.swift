70b6251086fd375afc9e021c5b1885fc231a64c4
Zeeshan Hooda <zeeshanhooda@icloud.com>
2019-06-06 12:07:42 -0600

    Initialized Xcode project files with SwiftUI.

---
+++ Sources/MobileClient/Dhicoin Client/Dhicoin Client/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Dhicoin Client
+//
+//  Created by Zeeshan Hooda on 6/6/19.
+//  Copyright © 2019 Deceptive Labs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ Sources/MobileClient/Dhicoin Client/Dhicoin Client/Data.swift
@@ -0,0 +1,23 @@
+//
+//  Data.swift
+//  Dhicoin Client
+//
+//  Created by Zeeshan Hooda on 6/6/19.
+//  Copyright © 2019 Deceptive Labs. All rights reserved.
+//
+
+import SwiftUI
+
+struct Data : View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+#if DEBUG
+struct Data_Previews : PreviewProvider {
+    static var previews: some View {
+        Data()
+    }
+}
+#endif
